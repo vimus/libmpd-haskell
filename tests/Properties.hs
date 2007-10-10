c6ad1f603e3e8fbec0391aba9d42706fa061c095
Joachim Fasting <joachim.fasting@gmail.com>
2007-10-10 20:28:50 +1000

    Add tests/Properties.hs.
    darcs-hash:20071010102850-928c4-fa438fd80b450099aa757b9f2faffac364fa513b.gz

---
+++ tests/Properties.hs
@@ -0,0 +1,34 @@
+{-# OPTIONS_GHC -fglasgow-exts -fno-warn-orphans #-}
+module Main (main) where
+import Network.MPD.Utils
+
+import Control.Monad
+import Data.Char
+import Data.List
+import Data.Maybe
+import System.Environment
+import Text.Printf
+import Test.QuickCheck
+
+main :: IO ()
+main = do
+    n <- (maybe 100 read . listToMaybe) `liftM` getArgs
+    mapM_ (\(s, f) -> printf "%-25s : " s >> f n) tests
+    where tests = [("splitGroups / idempotent",
+                        mytest prop_splitGroups_idem)
+                  ,("splitGroups / integrity",
+                        mytest prop_splitGroups_integrity)]
+
+mytest :: Testable a => a -> Int -> IO ()
+mytest a n = check defaultConfig { configMaxTest = n } a
+
+instance Arbitrary Char where
+    arbitrary     = choose ('\0', '\128')
+    coarbitrary c = variant (ord c `rem` 4)
+
+prop_splitGroups_idem :: [(String, String)] -> Bool
+prop_splitGroups_idem xs =
+    let r = splitGroups xs in r == splitGroups (concat r)
+
+prop_splitGroups_integrity :: [(String, String)] -> Bool
+prop_splitGroups_integrity xs = sort (concat $ splitGroups xs) == sort xs
