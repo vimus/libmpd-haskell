133e90877a328954f3758ecd7b3baf00958c18ef
Joachim Fasting <joachim.fasting@gmail.com>
2007-10-11 22:08:44 +1000

    Properties.hs: correct property names.
    darcs-hash:20071011120844-928c4-358fa186b51463accab04f46edb937d6e31af171.gz

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -14,13 +14,13 @@ main :: IO ()
 main = do
     n <- (maybe 100 read . listToMaybe) `liftM` getArgs
     mapM_ (\(s, f) -> printf "%-25s : " s >> f n) tests
-    where tests = [("splitGroups / idempotent",
-                        mytest prop_splitGroups_idem)
+    where tests = [("splitGroups / reversible",
+                        mytest prop_splitGroups_rev)
                   ,("splitGroups / integrity",
                         mytest prop_splitGroups_integrity)
                   ,("parseBool", mytest prop_parseBool)
-                  ,("toAssoc / idempotent",
-                        mytest prop_toAssoc_idem)
+                  ,("toAssoc / reversible",
+                        mytest prop_toAssoc_rev)
                   ,("toAssoc / integrity",
                         mytest prop_toAssoc_integrity)]
 
@@ -42,8 +42,8 @@ instance Arbitrary AssocString where
         return . AS $ key ++ ": " ++ val
     coarbitrary = undefined
 
-prop_toAssoc_idem :: [AssocString] -> Bool
-prop_toAssoc_idem x = toAssoc (fromAssoc r) == r
+prop_toAssoc_rev :: [AssocString] -> Bool
+prop_toAssoc_rev x = toAssoc (fromAssoc r) == r
     where r = toAssoc (fromAS x)
           fromAssoc = map (\(a, b) -> a ++ ": " ++ b)
 
@@ -56,8 +56,8 @@ fromAS s = [x | AS x <- s]
 prop_parseBool :: Bool -> Bool
 prop_parseBool x = parseBool (showBool x) == x
 
-prop_splitGroups_idem :: [(String, String)] -> Bool
-prop_splitGroups_idem xs =
+prop_splitGroups_rev :: [(String, String)] -> Bool
+prop_splitGroups_rev xs =
     let r = splitGroups xs in r == splitGroups (concat r)
 
 prop_splitGroups_integrity :: [(String, String)] -> Bool
