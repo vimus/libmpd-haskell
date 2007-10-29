1d7edc9a52b8d59472b01aa02195c604b2cf8369
Ben Sinclair <bsinclai@turing.une.edu.au>
2007-10-30 03:44:18 +1100

    MPD.hs: don't export testMPD.
    darcs-hash:20071029164418-87dc9-06d06fd50e87be8d04bb66d592720217319bcfb2.gz

--- Network/MPD.hs
+++ Network/MPD.hs
@@ -30,14 +30,13 @@ module Network.MPD (
     -- * Basic data types
     MPD, MPDError(..), ACKType(..), Response,
     -- * Connections
-    withMPD, withMPDEx, testMPD,
+    withMPD, withMPDEx,
     module Network.MPD.Commands,
     ) where
 
 import Network.MPD.Commands
 import Network.MPD.Prim
 import Network.MPD.SocketConn
-import Network.MPD.StringConn
 
 import Control.Monad (liftM)
 import Data.Maybe (listToMaybe)
