1a0590c8d27c1503a36616fbc7796c6a046d86f5
Ben Sinclair <bsinclai@turing.une.edu.au>
2007-11-12 01:02:50 +1100

    tests/Commands.hs: added some more tests.
    darcs-hash:20071111140250-87dc9-e3a3b2de0ce3e7c1557ff3d5d40192fe3d94ceb2.gz

--- tests/Commands.hs
+++ tests/Commands.hs
@@ -2,49 +2,85 @@ module Main (main) where
 
 import Network.MPD.StringConn
 import Network.MPD.Commands
+import Network.MPD.Prim (MPDError(..))
 
 import Data.Maybe
 import Control.Monad
 
-main = mapM_ (\(n, f) -> putStr (n ++ ": ") >> f) tests
+main = mapM_ (\(n, f) -> f >>= \x -> putStrLn (n ++ ": " ++ x)) tests
     where tests = [("play", testPlay)
                   ,("stop", testStop)
+                  ,("lsInfo", testLsInfo)
+                  ,("add_", testAdd_)
+                  ,("add", testAdd)
+                  ,("clear", testClear)
+                  ,("outputs", testOutputs)
+                  ,("disableOutput", testDisableOutput)
+                  ,("enableOutput", testEnableOutput)
+                  ,("update0", testUpdate0)
+                  ,("update1", testUpdate1)
+                  ,("updateMany", testUpdateMany)
+                  ,("ping", testPing)
+                  ,("urlHandlers", testUrlHandlers)
                   ]
 
-test a b c d = testMPD a b c d >>= print
+test a b c = liftM show $ testMPD a b (return Nothing) c
 
-testPlay = test [("play", Right "OK")] (Right ()) (return Nothing)
-               (play Nothing)
+test_ a b = test a (Right ()) b
 
-testStop = test [("stop", Right "OK")] (Right ()) (return Nothing)
-            stop
+testPlay = test_ [("play", Right "OK")] (play Nothing)
 
-testLsInfo = testMPD [("lsinfo \"\"", Right "directory: Foo\ndirectory: Bar\nOK")]
-                     (Right [Left "Bar", Left "Foo"])
-                     (return Nothing)
-                     (lsInfo "")
+testStop = test_ [("stop", Right "OK")] stop
 
-testAdd_ = testMPD [("add \"foo\"", Right "OK")]
-                    (Right ())
-                    (return Nothing)
-                    (add_ "" "foo")
+testLsInfo =
+    test [("lsinfo \"\"", Right "directory: Foo\ndirectory: Bar\nOK")]
+         (Right [Left "Bar", Left "Foo"])
+         (lsInfo "")
 
-testAdd = testMPD [("add \"foo\"", Right "OK"), ("listall \"foo\"", Right "file: Foo\nfile: Bar\nOK")]
-                  (Right ["Foo", "Bar"])
-                  (return Nothing)
-                  (add "" "foo")
+testAdd_ = test_ [("add \"foo\"", Right "OK")] (add_ "" "foo")
 
-testClear = testMPD [("playlistclear \"foo\"", Right "OK")] (Right ()) (return Nothing) (clear "foo")
+testAdd =
+    test [("add \"foo\"", Right "OK"),
+          ("listall \"foo\"", Right "file: Foo\nfile: Bar\nOK")]
+         (Right ["Foo", "Bar"])
+         (add "" "foo")
 
-testDisableOutput = testMPD
-    [("disableoutput 1", Right "OK")] (Right ()) (return Nothing) (disableOutput 1)
+testClear = test_ [("playlistclear \"foo\"", Right "OK")] (clear "foo")
 
-testEnableOutput = testMPD
-    [("enableoutput 1", Right "OK")] (Right ()) (return Nothing) (enableOutput 1)
+testOutputs =
+    test [("outputs", Right $ unlines ["outputid: 0"
+                                      ,"outputname: SoundCard0"
+                                      ,"outputenabled: 1"
+                                      ,"outputid: 1"
+                                      ,"outputname: SoundCard1"
+                                      ,"outputenabled: 0"
+                                      ,"OK"])]
+         (Right [Device { dOutputID = 0
+                        , dOutputName = "SoundCard0"
+                        , dOutputEnabled = True }
+                ,Device { dOutputID = 1
+                        , dOutputName = "SoundCard1"
+                        , dOutputEnabled = False }])
+         outputs
 
-testPing = testMPD [("ping", Right "OK")] (Right ()) (return Nothing) ping
+testDisableOutput = test_ [("disableoutput 1", Right "OK")] (disableOutput 1)
 
-testUrlHandlers = testMPD [("urlhandlers", Right "urlhandler: foo\nurlhandler: bar")]
-                  (Right ["foo", "bar"])
-                  (return Nothing)
-                  (urlHandlers)
+testEnableOutput = test_ [("enableoutput 1", Right "OK")] (enableOutput 1)
+
+testUpdate0 = test_ [("update", Right "updating_db: 1\nOK")] (update [])
+
+testUpdate1 =
+    test_ [("update \"foo\"", Right "updating_db: 1\nOK")]
+          (update ["foo"])
+
+testUpdateMany =
+    test_ [("command_list_begin\nupdate \"foo\"\nupdate \"bar\"\n\
+           \command_list_end", Right "updating_db: 1\nOK")]
+         (update ["foo","bar"])
+
+testPing = test_ [("ping", Right "OK")] ping
+
+testUrlHandlers =
+    test [("urlhandlers", Right "urlhandler: foo\nurlhandler: bar")]
+         (Right ["foo", "bar"])
+         (urlHandlers)
