624e0d011b43c91fa4c4bd5d48e07be426abb27b
Joachim Fasting <joachim.fasting@gmail.com>
2009-03-21 02:30:47 +1100

    Split argument preparation to separate module
    Ignore-this: 50a0325fdbc4ae4b7284b0bf16a9dfd
    
    darcs-hash:20090320153047-928c4-0de0cba19fde6e0ea6ab616106ac31ae6063b68e.gz

---
+++ Network/MPD/Arg.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
+-- | Module    : Network.MPD.Arg
+-- Copyright   : (c) Ben Sinclair 2005-2008
+-- License     : LGPL (see LICENSE)
+-- Maintainer  : bsinclai@turing.une.edu.au
+-- Stability   : alpha
+-- Portability : unportable (uses TypeSynonymInstances)
+--
+-- Prepare command arguments.
+
+module Network.MPD.Arg (Args(..), MPDArg(..), (<++>), (<$>)) where
+
+import Network.MPD.Utils (showBool)
+
+-- | Arguments for getResponse are accumulated as strings in values of
+-- this type after being converted from whatever type (an instance of
+-- MPDArg) they were to begin with.
+newtype Args = Args [String]
+    deriving Show
+
+-- | A uniform interface for argument preparation
+-- The basic idea is that one should be able
+-- to magically prepare an argument for use with
+-- an MPD command, without necessarily knowing/\caring
+-- how it needs to be represented internally.
+class Show a => MPDArg a where
+    prep :: a -> Args
+    -- Note that because of this, we almost
+    -- never have to actually provide
+    -- an implementation of 'prep'
+    prep = Args . return . show
+
+-- | Groups together arguments to getResponse.
+infixl 3 <++>
+(<++>) :: (MPDArg a, MPDArg b) => a -> b -> Args
+x <++> y = Args $ xs ++ ys
+    where Args xs = prep x
+          Args ys = prep y
+
+-- | Converts a command name and a string of arguments into the string
+-- to hand to getResponse.
+infix 2 <$>
+(<$>) :: (MPDArg a) => String -> a -> String
+x <$> y = x ++ " " ++ unwords (filter (not . null) y')
+    where Args y' = prep y
+
+instance MPDArg Args where prep = id
+
+instance MPDArg String where
+    -- We do this to avoid mangling
+    -- non-ascii characters with 'show'
+    prep x = Args ['"' : x ++ "\""]
+
+instance (MPDArg a) => MPDArg (Maybe a) where
+    prep Nothing = Args []
+    prep (Just x) = prep x
+
+instance MPDArg Int
+instance MPDArg Integer
+instance MPDArg Bool where prep = Args . return . showBool
--- Network/MPD/Commands.hs
+++ Network/MPD/Commands.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE PatternGuards, TypeSynonymInstances #-}
+{-# LANGUAGE PatternGuards #-}
 
 -- | Module    : Network.MPD.Commands
 -- Copyright   : (c) Ben Sinclair 2005-2008
 -- License     : LGPL (see LICENSE)
 -- Maintainer  : bsinclai@turing.une.edu.au
 -- Stability   : alpha
--- Portability : unportable (uses PatternGuards and TypeSynonymInstances)
+-- Portability : unportable (uses PatternGuards)
 --
 -- Interface to the user commands supported by MPD.
 
@@ -42,6 +42,7 @@ module Network.MPD.Commands (
     searchArtist, searchAlbum, searchTitle, getPlaylist, toggle, updateId
     ) where
 
+import Network.MPD.Arg
 import Network.MPD.Core
 import Network.MPD.Utils
 import Network.MPD.Parse
@@ -58,53 +59,6 @@ import System.FilePath (dropFileName)
 --
 -- Data types
 --
-
--- Arguments for getResponse are accumulated as strings in values of
--- this type after being converted from whatever type (an instance of
--- MPDArg) they were to begin with.
-newtype Args = Args [String]
-    deriving Show
-
--- A uniform interface for argument preparation
--- The basic idea is that one should be able
--- to magically prepare an argument for use with
--- an MPD command, without necessarily knowing/\caring
--- how it needs to be represented internally.
-class Show a => MPDArg a where
-    prep :: a -> Args
-    -- Note that because of this, we almost
-    -- never have to actually provide
-    -- an implementation of 'prep'
-    prep = Args . return . show
-
--- | Groups together arguments to getResponse.
-infixl 3 <++>
-(<++>) :: (MPDArg a, MPDArg b) => a -> b -> Args
-x <++> y = Args $ xs ++ ys
-    where Args xs = prep x
-          Args ys = prep y
-
--- | Converts a command name and a string of arguments into the string
--- to hand to getResponse.
-infix 2 <$>
-(<$>) :: (MPDArg a) => String -> a -> String
-x <$> y = x ++ " " ++ unwords (filter (not . null) y')
-    where Args y' = prep y
-
-instance MPDArg Args where prep = id
-
-instance MPDArg String where
-    -- We do this to avoid mangling
-    -- non-ascii characters with 'show'
-    prep x = Args ['"' : x ++ "\""]
-
-instance (MPDArg a) => MPDArg (Maybe a) where
-    prep Nothing = Args []
-    prep (Just x) = prep x
-
-instance MPDArg Int
-instance MPDArg Integer
-instance MPDArg Bool where prep = Args . return . showBool
 
 type Artist       = String
 type Album        = String
