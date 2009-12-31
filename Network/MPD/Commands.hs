bcfaa7d3402ec9eb835332fd851177876991658c
Joachim Fasting <joachim.fasting@gmail.com>
2009-12-31 11:12:48 +0100

    Add sticker commands
    Signed-off-by: Joachim Fasting <joachim.fasting@gmail.com>

--- Network/MPD/Commands.hs
+++ Network/MPD/Commands.hs
@@ -38,6 +38,9 @@ module Network.MPD.Commands (
     clearError, commands, notCommands, password, ping, stats, status,
     tagTypes, urlHandlers, decoders,
 
+    -- * Stickers
+    stickerGet, stickerSet, stickerDelete, stickerList, stickerFind,
+
     -- * Extensions\/shortcuts
     addMany, deleteMany, complete, crop, prune, lsDirs, lsFiles, lsPlaylists,
     listArtists, listAlbums, listAlbum, getPlaylist, toggle, updateId, volume
@@ -435,6 +438,50 @@ stats = getResponse "stats" >>= runParser parseStats
 -- | Get the server's status.
 status :: MonadMPD m => m Status
 status = getResponse "status" >>= runParser parseStatus
+
+--
+-- Stickers
+--
+
+-- | Reads a sticker value for the specified object.
+stickerGet :: MonadMPD m => ObjectType
+           -> String -- ^ Object URI
+           -> String -- ^ Sticker name
+           -> m [String]
+stickerGet typ uri name = takeValues `liftM` getResponse ("sticker get" <$> typ <++> uri <++> name)
+
+-- | Adds a sticker value to the specified object.
+stickerSet :: MonadMPD m => ObjectType
+           -> String -- ^ Object URI
+           -> String -- ^ Sticker name
+           -> String -- ^ Sticker value
+           -> m ()
+stickerSet typ uri name value =
+    getResponse_ ("sticker set" <$> typ <++> uri <++> name <++> value)
+
+-- | Delete a sticker value from the specified object.
+stickerDelete :: MonadMPD m => ObjectType
+              -> String -- ^ Object URI
+              -> String -- ^ Sticker name
+              -> m ()
+stickerDelete typ uri name =
+    getResponse_ ("sticker delete" <$> typ <++> uri <++> name)
+
+-- | Lists the stickers for the specified object.
+stickerList :: MonadMPD m => ObjectType
+            -> String -- ^ Object URI
+            -> m [(String, String)] -- ^ Sticker name\/sticker value
+stickerList typ uri =
+    toAssocList `liftM` getResponse ("sticker list" <$> typ <++> uri)
+
+-- | Searches the sticker database for stickers with the specified name, below
+-- the specified path.
+stickerFind :: MonadMPD m => ObjectType
+            -> String -- ^ Path
+            -> String -- ^ Sticker name
+            -> m [(String, String)] -- ^ URI\/sticker value
+stickerFind typ uri name =
+    toAssocList `liftM` getResponse ("sticker find" <$> typ <++> uri <++> name)
 
 --
 -- Extensions\/shortcuts.
