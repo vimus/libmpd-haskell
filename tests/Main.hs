0df412bddac13d76061b300c338b6ee79a3a1ca3
Joachim Fasting <joachim.fasting@gmail.com>
2012-03-28 09:30:32 +0200

    Migrate tests/Commands.hs to hspec

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import qualified CommandSpec
 import qualified EnvSpec
 import qualified ParserSpec
 import qualified UtilSpec
@@ -7,6 +8,7 @@ import           Test.Hspec.Monadic (describe, hspecX)
 
 main :: IO ()
 main = hspecX $ do
+    describe "CommandSpec" CommandSpec.spec
     describe "EnvSpec" EnvSpec.spec
     describe "ParserSpec" ParserSpec.spec
     describe "UtilSpec" UtilSpec.spec
