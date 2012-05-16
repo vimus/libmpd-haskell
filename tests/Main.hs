e6e5ae6221ff1b0b11dd06f0ba2dbed0fea11721
Simon Hengel <sol@typeful.net>
2012-05-16 08:26:32 +0200

    Use hspec-discover for test driver

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,14 +1,1 @@
-module Main (main) where
-
-import qualified CommandSpec
-import qualified EnvSpec
-import qualified ParserSpec
-import qualified UtilSpec
-import           Test.Hspec.Monadic (describe, hspecX)
-
-main :: IO ()
-main = hspecX $ do
-    describe "CommandSpec" CommandSpec.spec
-    describe "EnvSpec" EnvSpec.spec
-    describe "ParserSpec" ParserSpec.spec
-    describe "UtilSpec" UtilSpec.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
