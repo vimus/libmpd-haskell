6309e0a9b512c6e9e1dd53904f6931d820bd3651
Joachim Fasting <joachim.fasting@gmail.com>
2012-06-29 08:26:06 +0200

    Convert StoredPlaylist to applicative style

---
+++ Network/MPD/Applicative/StoredPlaylists.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{- |
+Module      : Network.MPD.Applicative.StoredPlaylists
+Copyright   : (c) Joachim Fasting 2012
+License     : MIT
+
+Maintainer  : joachim.fasting@gmail.com
+Stability   : stable
+Portability : unportable
+
+Stored playlists.
+-}
+
+module Network.MPD.Applicative.StoredPlaylists
+    ( listPlaylist
+    , listPlaylistInfo
+    , listPlaylists
+    , load
+    , playlistAdd
+    , playlistClear
+    , playlistDelete
+    , playlistMove
+    , rename
+    , rm
+    , save
+    ) where
+
+import           Network.MPD.Applicative
+import           Network.MPD.Applicative.Util
+import           Network.MPD.Commands.Arg hiding (Command)
+import           Network.MPD.Commands.Types
+import           Network.MPD.Commands.Util (takeValues)
+import           Network.MPD.Util
+
+import           Control.Applicative
+
+-- | List song items in the playlist.
+listPlaylist :: PlaylistName -> Command [Path]
+listPlaylist plName = Command p ["listplaylist" <@> plName]
+    where
+        p = map Path . takeValues <$> getResponse
+
+-- | List song items in the playlist with metadata.
+listPlaylistInfo :: PlaylistName -> Command [Song]
+listPlaylistInfo plName =
+    Command (liftParser takeSongs) ["listplaylistinfo" <@> plName]
+
+-- | Get a list of stored playlists.
+listPlaylists :: Command [PlaylistName]
+listPlaylists = Command p ["listplaylists"]
+    where
+        p = map PlaylistName . go [] . toAssocList <$> getResponse
+
+        -- XXX: need to fail gracefully here
+        -- After each playlist name we get a timestamp
+        go acc [] = acc
+        go acc ((_, b):_:xs) = go (b : acc) xs
+        go _ _ = error "listPlaylists: bug"
+
+-- | Load playlist into the current queue.
+load :: PlaylistName -> Command ()
+load plName = Command emptyResponse ["load" <@> plName]
+
+-- | Add a database path to the named playlist.
+playlistAdd :: PlaylistName -> Path -> Command ()
+playlistAdd plName path =
+    Command emptyResponse ["playlistadd" <@> plName <++> path]
+
+-- | Clear the playlist.
+playlistClear :: PlaylistName -> Command ()
+playlistClear plName = Command emptyResponse ["playlistclear" <@> plName]
+
+-- | Delete the item at the given position from the playlist.
+playlistDelete :: PlaylistName -> Position -> Command ()
+playlistDelete name pos =
+    Command emptyResponse ["playlistdelete" <@> name <++> pos]
+
+-- | Move a song to a new position within the playlist.
+playlistMove :: PlaylistName -> Id -> Position -> Command ()
+playlistMove name from to =
+    Command emptyResponse ["playlistmove" <@> name <++> from <++> to]
+
+-- | Rename the playlist.
+rename :: PlaylistName -> PlaylistName -> Command ()
+rename plName new = Command emptyResponse ["rename" <@> plName <++> new]
+
+-- | Remove the playlist.
+rm :: PlaylistName -> Command ()
+rm plName = Command emptyResponse ["rm" <@> plName]
+
+-- | Save current queue to the named playlist.
+save :: PlaylistName -> Command ()
+save plName = Command emptyResponse ["save" <@> plName]
---
+++ tests/Network/MPD/Applicative/StoredPlaylistsSpec.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Network.MPD.Applicative.StoredPlaylistsSpec (main, spec) where
+
+import           TestUtil
+import           Unparse
+
+import           Network.MPD.Applicative.StoredPlaylists
+import           Network.MPD.Commands.Types
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    describe "listPlaylist" $ do
+        it "returns a list of uris in a playlist" $ do
+            listPlaylist "foo"
+                `with` [("listplaylist \"foo\""
+                       , Right "file: foo.ogg\n\
+                               \file: bar.ogg\n\
+                               \OK")]
+                `shouldBe` Right ["foo.ogg", "bar.ogg"]
+
+    describe "listPlaylistInfo" $ do
+        it "returns metadata for songs in a playlist" $ do
+            let obj = defaultSong "foo.ogg"
+            listPlaylistInfo "foo"
+                `with` [("listplaylistinfo \"foo\""
+                       , Right (unparse obj ++ "\nOK")
+                       )]
+                `shouldBe` Right [obj]
+
+    describe "listPlaylists" $ do
+        it "returns a list of playlists" $ do
+            listPlaylists
+                `with` [("listplaylists"
+                       , Right "playlist: foo\n\
+                               \Last-Modified: 2011-12-03T08:53:09Z\n\
+                               \playlist: bar\n\
+                               \Last-Modified: 2012-12-03T09:57:09Z\n\
+                               \OK"
+                       )]
+                `shouldBe` Right ["bar", "foo"]
+
+    describe "load" $ do
+        it "sends a load request" $ do
+            load "foo" `with` [("load \"foo\"", Right "OK")]
+                `shouldBe` Right ()
+
+    describe "playlistAdd" $ do
+        it "sends a playlistadd request" $ do
+            playlistAdd "foo" "foo.ogg"
+                `with` [("playlistadd \"foo\" \"foo.ogg\"", Right "OK")]
+                `shouldBe` Right ()
+
+    describe "playlistClear" $ do
+        it "sends a playlistclear request" $ do
+            playlistClear "foo"
+                `with` [("playlistclear \"foo\"", Right "OK")]
+                `shouldBe` Right ()
+
+    describe "playlistDelete" $ do
+        it "sends a playlistdelete request" $ do
+            playlistDelete "foo" 10
+                `with` [("playlistdelete \"foo\" 10", Right "OK")]
+                `shouldBe` Right ()
+
+    describe "playlistMove" $ do
+        it "sends a playlistmove request" $ do
+            playlistMove "foo" (Id 10) 20
+                `with` [("playlistmove \"foo\" 10 20", Right "OK")]
+                `shouldBe` Right ()
+
+    describe "rename" $ do
+        it "sends a rename request" $ do
+            rename "foo" "bar"
+                `with` [("rename \"foo\" \"bar\"", Right "OK")]
+                `shouldBe` Right ()
+
+    describe "save" $ do
+        it "sends a save request" $ do
+            save "foo"
+                `with` [("save \"foo\"", Right "OK")]
+                `shouldBe` Right ()
+
