1e77fb4cec5a766ad2b82df992de43779207123b
Joachim Fasting <joachifm@fastmail.fm>
2014-04-18 18:51:24 +0200

    Simplify default.nix

--- default.nix
+++ default.nix
@@ -1,18 +1,10 @@
-{}:
-with import <nixpkgs>{};
-
-let
-  haskell = haskellPackages;
-  inherit (haskell) cabal cabalInstall dataDefault filepath mtl attoparsec
-  network text time utf8String;
-in
-
-cabal.mkDerivation (self: {
+{ haskellPackages ? (import <nixpkgs>{}).haskellPackages_ghc782.profiling }:
+with haskellPackages; cabal.mkDerivation (self: {
   pname = "libmpd";
   version = "0.9.0";
   src = ./.;
   isLibrary = true;
-  isExecutable = true;
+  isExecutable = false;
   propagatedDependencies = [ cabalInstall ];
   buildDepends = [
     attoparsec dataDefault filepath mtl network text time utf8String
