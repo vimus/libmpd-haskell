vimus/libmpd-haskell

commit 55de3e9e02daeff87651853d7713401c3339da24

Handle cases where the output has no mixer
stVolume is now a Maybe, where Nothing indicates that
the output has no mixer support.

src/Network/MPD/Applicative/Status.hs
@@ -82,7 +82,7 @@ status = Command (liftParser parseStatus) ["status"]
     parseStatus = foldM go def . toAssocList
         where
             go a p@(k, v) = case k of
-                "volume"         -> num   $ \x -> a { stVolume          = x }
+                "volume"         -> vol   $ \x -> a { stVolume          = x }
                 "repeat"         -> bool  $ \x -> a { stRepeat          = x }
                 "random"         -> bool  $ \x -> a { stRandom          = x }
                 "single"         -> bool  $ \x -> a { stSingle          = x }
@@ -123,3 +123,8 @@ status = Command (liftParser parseStatus) ["status"]
                         "pause" -> (Right . f) Paused
                         "stop"  -> (Right . f) Stopped
                         _       -> unexpectedPair
+
+                    -- A volume of -1 indicates an audio backend w/o a mixer
+                    vol f = Right . f . join . fmap g $ parseNum v
+                      where g n | n < 0     = Nothing
+                                | otherwise = Just n

src/Network/MPD/Commands/Extensions.hs
@@ -100,5 +100,9 @@ getPlaylist = playlistInfo Nothing
 -- 'volume (-10)' will decrease it by the same amount.
 volume :: MonadMPD m => Int -> m ()
 volume n = do
-    current <- (fromIntegral . stVolume) `liftM` status
-    setVolume . round $ (fromIntegral n / (100 :: Double)) * current + current
+    cur <- stVolume `liftM` status
+    case cur of
+        Nothing -> return ()
+        Just v  -> setVolume (adjust v)
+    where
+        adjust x = round $ (fromIntegral n / (100 :: Double)) * x + x

src/Network/MPD/Commands/Types.hs
@@ -281,8 +281,10 @@ instance Default Stats where
 -- | Container for MPD status.
 data Status =
     Status { stState :: State
-             -- | A percentage (0-100)
-           , stVolume          :: Int
+             -- | A percentage (0-100).
+             --
+             -- 'Nothing' indicates that the output lacks mixer support.
+           , stVolume          :: Maybe Int
            , stRepeat          :: Bool
            , stRandom          :: Bool
              -- | A value that is incremented by the server every time the
@@ -323,7 +325,7 @@ data Status =
 
 defaultStatus :: Status
 defaultStatus =
-    Status { stState = Stopped, stVolume = 0, stRepeat = False
+    Status { stState = Stopped, stVolume = Just 0, stRepeat = False
            , stRandom = False, stPlaylistVersion = 0, stPlaylistLength = 0
            , stSongPos = Nothing, stSongID = Nothing, stTime = (0,0)
            , stNextSongPos = Nothing, stNextSongID = Nothing

tests/CommandSpec.hs
@@ -403,4 +403,4 @@ testAddMany1 = cmd_ [("playlistadd \"foo\" \"bar\"", Right "OK")]
                (addMany "foo" ["bar"])
 
 testVolume = cmd_ [("status", Right st), ("setvol 90", Right "OK")] (volume (-10))
-    where st = unparse def { stVolume = 100 }
+    where st = unparse def { stVolume = Just 100 }

commit 9c07d23fca8ba796a30017d6f1b26658d6a80d27

Extensions (volume): fix type error
Fixes type error introduced by
55de3e9e02daeff87651853d7713401c3339da24

src/Network/MPD/Applicative/Status.hs
@@ -127,4 +127,4 @@ status = Command (liftParser parseStatus) ["status"]
                     -- A volume of -1 indicates an audio backend w/o a mixer
                     vol f = Right . f . join . fmap g $ parseNum v
                       where g n | n < 0     = Nothing
-                                | otherwise = Just n
+                                | otherwise = Just (n::Int)

src/Network/MPD/Commands/Extensions.hs
@@ -105,4 +105,5 @@ volume n = do
         Nothing -> return ()
         Just v  -> setVolume (adjust v)
     where
-        adjust x = round $ (fromIntegral n / (100 :: Double)) * x + x
+        adjust x = round $ (fromIntegral n / (100 :: Double)) * x' + x'
+          where x' = fromIntegral x