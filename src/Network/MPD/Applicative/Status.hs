vimus/libmpd-haskell

commit 941b5f706581a058d0bbd1b745181f5542ba0de9

Wrap stBitrate and stTime in Maybe
Providing a value for either when no song is
playing makes no sense.

src/Network/MPD/Applicative/Status.hs
@@ -94,9 +94,9 @@ status = Command (liftParser parseStatus) ["status"]
                 "songid"         -> num   $ \x -> a { stSongID          = Just $ Id x }
                 "nextsong"       -> num   $ \x -> a { stNextSongPos     = Just x }
                 "nextsongid"     -> num   $ \x -> a { stNextSongID      = Just $ Id x }
-                "time"           -> time  $ \x -> a { stTime            = x }
-                "elapsed"        -> frac  $ \x -> a { stTime            = (x, snd $ stTime a) }
-                "bitrate"        -> num   $ \x -> a { stBitrate         = x }
+                "time"           -> time  $ \x -> a { stTime            = Just x }
+                "elapsed"        -> frac  $ \x -> a { stTime            = fmap ((,) x . snd) (stTime a) }
+                "bitrate"        -> num   $ \x -> a { stBitrate         = Just x }
                 "xfade"          -> num   $ \x -> a { stXFadeWidth      = x }
                 "mixrampdb"      -> frac  $ \x -> a { stMixRampdB       = x }
                 "mixrampdelay"   -> frac  $ \x -> a { stMixRampDelay    = x }

src/Network/MPD/Commands/Types.hs
@@ -300,10 +300,10 @@ data Status =
            , stNextSongPos     :: Maybe Position
              -- | Next song's playlist ID.
            , stNextSongID      :: Maybe Id
-             -- | Time elapsed\/total time.
-           , stTime            :: (Double, Seconds)
+             -- | Time elapsed\/total time of playing song (if any).
+           , stTime            :: Maybe (Double, Seconds)
              -- | Bitrate (in kilobytes per second) of playing song (if any).
-           , stBitrate         :: Int
+           , stBitrate         :: Maybe Int
              -- | Crossfade time.
            , stXFadeWidth      :: Seconds
              -- | MixRamp threshold in dB
@@ -327,9 +327,9 @@ defaultStatus :: Status
 defaultStatus =
     Status { stState = Stopped, stVolume = Just 0, stRepeat = False
            , stRandom = False, stPlaylistVersion = 0, stPlaylistLength = 0
-           , stSongPos = Nothing, stSongID = Nothing, stTime = (0,0)
+           , stSongPos = Nothing, stSongID = Nothing, stTime = Nothing
            , stNextSongPos = Nothing, stNextSongID = Nothing
-           , stBitrate = 0, stXFadeWidth = 0, stMixRampdB = 0
+           , stBitrate = Nothing, stXFadeWidth = 0, stMixRampdB = 0
            , stMixRampDelay = 0, stAudio = (0,0,0), stUpdatingDb = Nothing
            , stSingle = False, stConsume = False, stError = Nothing }
 

tests/Unparse.hs
@@ -60,10 +60,7 @@ instance Unparse Status where
         , "playlist: " ++ show (stPlaylistVersion s)
         , "playlistlength: " ++ show (stPlaylistLength s)
         , "xfade: " ++ show (stXFadeWidth s)
-        , "time: " ++ (let (x, y) = stTime s in show x ++ ":" ++ show y)
-        , "bitrate: " ++ show (stBitrate s)
         , "xfade: " ++ show (stXFadeWidth s)
-
         , "audio: " ++ (let (x, y, z) = stAudio s in show x ++ ":" ++ show y ++
                                        ":" ++ show z)
         , "single: " ++ showBool (stSingle s)
@@ -73,3 +70,5 @@ instance Unparse Status where
         ++ maybe [] (\n -> ["song: " ++ show n]) (stSongPos s)
         ++ maybe [] (\n -> ["songid: " ++ show n]) (stSongID s)
         ++ maybe [] (\n -> ["error: " ++ show n]) (stError s)
+        ++ maybe [] (\(x, y) -> ["time: " ++ show x ++ ":" ++ show y]) (stTime  s)
+        ++ maybe [] (\n -> ["bitrate: " ++ show n]) (stBitrate s)

commit bb11c34fe27d039e990a3d40c84447de490cd1f2

Status: fix type error
Fix type errors introduced by
941b5f706581a058d0bbd1b745181f5542ba0de9

src/Network/MPD/Applicative/Status.hs
@@ -89,14 +89,14 @@ status = Command (liftParser parseStatus) ["status"]
                 "consume"        -> bool  $ \x -> a { stConsume         = x }
                 "playlist"       -> num   $ \x -> a { stPlaylistVersion = x }
                 "playlistlength" -> num   $ \x -> a { stPlaylistLength  = x }
-                "state"          -> state $ \x -> a { stState           = x}
-                "song"           -> num   $ \x -> a { stSongPos         = Just x }
-                "songid"         -> num   $ \x -> a { stSongID          = Just $ Id x }
-                "nextsong"       -> num   $ \x -> a { stNextSongPos     = Just x }
-                "nextsongid"     -> num   $ \x -> a { stNextSongID      = Just $ Id x }
+                "state"          -> state $ \x -> a { stState           = x }
+                "song"           -> int   $ \x -> a { stSongPos         = Just x }
+                "songid"         -> int   $ \x -> a { stSongID          = Just $ Id x }
+                "nextsong"       -> int   $ \x -> a { stNextSongPos     = Just x }
+                "nextsongid"     -> int   $ \x -> a { stNextSongID      = Just $ Id x }
                 "time"           -> time  $ \x -> a { stTime            = Just x }
                 "elapsed"        -> frac  $ \x -> a { stTime            = fmap ((,) x . snd) (stTime a) }
-                "bitrate"        -> num   $ \x -> a { stBitrate         = Just x }
+                "bitrate"        -> int   $ \x -> a { stBitrate         = Just x }
                 "xfade"          -> num   $ \x -> a { stXFadeWidth      = x }
                 "mixrampdb"      -> frac  $ \x -> a { stMixRampdB       = x }
                 "mixrampdelay"   -> frac  $ \x -> a { stMixRampDelay    = x }
@@ -106,6 +106,7 @@ status = Command (liftParser parseStatus) ["status"]
                 _                -> unexpectedPair
                 where
                     unexpectedPair = Left ("unexpected key-value pair: " ++ show p)
+                    int   f = maybe unexpectedPair (Right . f) (parseNum v :: Maybe Int)
                     num   f = maybe unexpectedPair (Right . f) (parseNum  v)
                     bool  f = maybe unexpectedPair (Right . f) (parseBool v)
                     frac  f = maybe unexpectedPair (Right . f) (parseFrac v)