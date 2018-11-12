20ad4017aed29196b98d53241d73277a391d983b
Andrzej Ressel <jereksel@gmail.com>
2018-11-12 19:01:44 +0100

    Better logging

--- tests/ParserSpec.hs
+++ tests/ParserSpec.hs
@@ -41,25 +41,25 @@ spec = do
 -- This property also ensures, that (instance Arbitrary UTCTime) is sound.
 -- Indeed, a bug in the instance declaration was the primary motivation to add
 -- this property.
-prop_parseIso8601 :: UTCTime -> Bool
-prop_parseIso8601 t = Just t == (parseIso8601 . UTF8.fromString . formatIso8601) t
+prop_parseIso8601 :: UTCTime -> Expectation
+prop_parseIso8601 t = Just t `shouldBe` (parseIso8601 . UTF8.fromString . formatIso8601) t
 
-prop_parseCount :: Count -> Bool
-prop_parseCount c = Right c == (parseCount . map UTF8.fromString . lines . unparse) c
+prop_parseCount :: Count -> Expectation
+prop_parseCount c = Right c `shouldBe` (parseCount . map UTF8.fromString . lines . unparse) c
 
-prop_parseOutputs :: [Device] -> Bool
+prop_parseOutputs :: [Device] -> Expectation
 prop_parseOutputs ds =
-    Right ds == (parseOutputs . map UTF8.fromString . lines . concatMap unparse) ds
+    Right ds `shouldBe` (parseOutputs . map UTF8.fromString . lines . concatMap unparse) ds
 
 deriving instance Ord Value
 
-prop_parseSong :: Song -> Bool
-prop_parseSong s = Right (sortTags s) == sortTags `fmap` (parseSong . toAssocList . map UTF8.fromString . lines . unparse) s
+prop_parseSong :: Song -> Expectation
+prop_parseSong s = Right (sortTags s) `shouldBe` sortTags `fmap` (parseSong . toAssocList . map UTF8.fromString . lines . unparse) s
   where
     -- We consider lists of tag values equal if they contain the same elements.
     -- To ensure that two lists with the same elements are equal, we bring the
     -- elements in a deterministic order.
     sortTags song = song { sgTags = M.map sort $ sgTags song }
 
-prop_parseStats :: Stats -> Bool
-prop_parseStats s = Right s == (parseStats . map UTF8.fromString . lines . unparse) s
+prop_parseStats :: Stats -> Expectation
+prop_parseStats s = Right s `shouldBe` (parseStats . map UTF8.fromString . lines . unparse) s
