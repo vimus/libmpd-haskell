ffa76eee19dce091f7961c81c787f479caea8d45
Itai Y. Efrat <itai3397@gmail.com>
2020-09-06 17:03:25 +0300

    add Sign newtype
    Needed to be able to pass signed numbers in commands,
    as doing `prep 3` will give "3" and not "+3".
    Used in the relative time option of seekcur

--- src/Network/MPD/Commands/Arg.hs
+++ src/Network/MPD/Commands/Arg.hs
@@ -12,7 +12,7 @@ Portability : unportable
 Prepare command arguments.
 -}
 
-module Network.MPD.Commands.Arg (Command, Args(..), MPDArg(..), (<++>), (<@>)) where
+module Network.MPD.Commands.Arg (Command, Args(..), MPDArg(..), (<++>), (<@>),Sign(..)) where
 
 import           Network.MPD.Util (showBool)
 
@@ -76,6 +76,15 @@ instance MPDArg Int
 instance MPDArg Integer
 instance MPDArg Bool where prep = Args . return . showBool
 instance MPDArg Double
+
+-- | wrapper for creating signed instances of MPDArg
+newtype Sign a = Sign {unSign :: a}
+  deriving (Show)
+
+instance (Num a,Ord a,Show a) => MPDArg (Sign a) where
+  prep sx | x >= 0 = Args ["+" ++ show x]
+          | otherwise  = Args [show x]
+    where x = unSign sx
 
 addSlashes :: String -> String
 addSlashes = concatMap escapeSpecial
